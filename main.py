4e8b93148641ef0116762f1fed310fcaf6db56b9
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-16 16:17:51 +0500

    added if and else statements

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 #Rock Paper Scissor game
 import random
+winner = ''
 random_choice = random.randint(0, 2)
-print('The computer chooses', random_choice)
+if random_choice == 0:
+  choice_computer = 'rock'
+elif random_choice == 1:
+  choice_computer = 'paper'
+else:
+  choice_computer = 'scissor' 
+user_choice = input('rock, paper or scissors? ')
+if choice_computer == user_choice:
+  winner = 'Tie'
+elif choice_computer == 'paper' and user_choice == 'rock':
+  winner = 'Computer'
+elif choice_computer == 'rock' and user_choice == 'scissors':
+  winner = 'Computer'
+elif choice_computer == 'scissors' and user_choice == 'paper':
+  winner = 'Computer'
+else:
+  winner = 'User'  
+if winner == 'Tie':
+  print('We both chose', choice_computer + ', play again.')
+else:
+  print(winner, 'won. The computer chose', choice_computer + '.')
+
+
+
 
 #practice
 bank_balance = 50
