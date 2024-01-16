6c7fb58e14e99a0c53c519f3f02dbbc380916a67
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-16 14:58:41 +0500

    practice no.2 done

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 random_choice = random.randint(0, 2)
 print('The computer chooses', random_choice)
 
-
 #practice
 bank_balance = 50
 ferrari_cost = 20
@@ -16,3 +15,17 @@ if bank_balance >= ferrari_cost:
 else:
   print('Sorry')
   print('Try again next week')
+
+#practice no.2
+number_of_scoops = input('Enter the number of scoops of ice cream you would like to get') 
+if int(number_of_scoops) == 0:
+  print("You didn't want any ice cream?")
+  print('We have lots of flavors.')
+elif int(number_of_scoops) == 1:
+  print('A single scoop for you, coming up.')
+elif int(number_of_scoops) == 2:
+  print('Oh, two scoops for you!')
+elif int(number_of_scoops) >= 3:
+  print("Wow, that's a lot of scoops!")
+else:
+  print("I'm sorry I can't give you negative scoops.")
