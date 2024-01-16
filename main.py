6f52f8b75e81c3baaf38b618828f1a735a0a2380
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-16 14:47:16 +0500

    doing practice

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 import random
 random_choice = random.randint(0, 2)
 print('The computer chooses', random_choice)
+
+
+#practice
+bank_balance = 50
+ferrari_cost = 20
+decision = bank_balance > ferrari_cost
+print(decision)
+
+if bank_balance >= ferrari_cost:
+  print('Why not?')
+  print('Go ahead, buy it')
+else:
+  print('Sorry')
+  print('Try again next week')
